5cac462102d3ca5f2fa8320354475939fa83479a
Megha Patel <htmldesigner@penguinpeak.com>
2026-02-13 11:05:10 +0530

    Learned about components

--- resources/views/component.blade.php
+++ resources/views/component.blade.php
@@ -1,29 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <div class="header">
-    <ul>
-        <li>
-            
-        <a href="">about</a>
-    
-    </li>
-     <li>
-            
-        <a href="">contact</a>
-    
-    </li> 
-    <li>
-            
-        <a href="">faq</a>
-    
-    </li>
-    </ul>
-    </div>
-</body>
-</html>
+
+<x-layout>
+   <x-slot name="title">Homepage</x-slot> 
+</x-layout>
