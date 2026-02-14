ec79cdd09c0d8127a214be152c3b54f6b097235d
project-demo <kamleshsahsani@gmail.com>
2026-02-14 12:22:49 +0530

    Learned accessorsd

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    function getNameAttribute($val){
+        return ucfirst($val);
+    }
+    function getPhoneAttribute($val){
+        return "+91".$val;
+    }
 }
